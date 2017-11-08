8a29d37eeb2d6b9f2096325228840fa242fa16dc
radia <nicklas.oest@gmail.com>
2017-11-08 19:58:20 +0100

    update

--- src/se/nios/scrum/calculator/CalculatorService.java
+++ src/se/nios/scrum/calculator/CalculatorService.java
@@ -1,111 +1,58 @@
 package se.nios.scrum.calculator;
 
 import java.math.BigInteger;
-import java.util.InputMismatchException;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class CalculatorService {
+    private Map<String,Double> mMemory;
+
+    public CalculatorService(){
+        mMemory = new HashMap<>();
+    }
 
 
     public double add(String first, String second) throws InvalidCalculatorInputFormat {
         Locale locale = Locale.getDefault();
         double firstValue = 0D;
         double secondValue = 0D;
-
-        try{
-            Scanner scanner = new Scanner(first);
-            scanner.useLocale(locale);
-            firstValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-        }
-
-        try{
-            Scanner scanner = new Scanner(second);
-            scanner.useLocale(locale);
-            secondValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-
-        }
-
-
-
-        return firstValue + secondValue;
+        firstValue = getFormattedValue(first, locale);
+        secondValue = getFormattedValue(second, locale);
+        double calcValue = firstValue + secondValue;
+        mMemory.put("m"+mMemory.size(),calcValue);
+        return calcValue;
     }
 
     public double subtract(String first, String second) throws InvalidCalculatorInputFormat {
         Locale locale = Locale.getDefault();
         double firstValue = 0D;
         double secondValue = 0D;
-
-        try{
-            Scanner scanner = new Scanner(first);
-            scanner.useLocale(locale);
-            firstValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-        }
-
-        try{
-            Scanner scanner = new Scanner(second);
-            scanner.useLocale(locale);
-            secondValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-
-        }
-        return firstValue - secondValue;
+        firstValue = getFormattedValue(first, locale);
+        secondValue = getFormattedValue(second, locale);
+        double calcValue = firstValue - secondValue;
+        mMemory.put("m"+mMemory.size(),calcValue);
+        return calcValue;
     }
 
     public double multiply(String first, String second) throws InvalidCalculatorInputFormat {
         Locale locale = Locale.getDefault();
         double firstValue = 0D;
         double secondValue = 0D;
-
-        try{
-            Scanner scanner = new Scanner(first);
-            scanner.useLocale(locale);
-            firstValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-        }
-
-        try{
-            Scanner scanner = new Scanner(second);
-            scanner.useLocale(locale);
-            secondValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-
-        }
-
-        return firstValue * secondValue;
+        firstValue = getFormattedValue(first, locale);
+        secondValue = getFormattedValue(second, locale);
+        double calcValue = firstValue * secondValue;
+        mMemory.put("m"+mMemory.size(),calcValue);
+        return calcValue;
     }
 
     public double divide(String first, String second) throws InvalidCalculatorInputFormat {
         Locale locale = Locale.getDefault();
         double firstValue = 0D;
         double secondValue = 0D;
-
-        try{
-            Scanner scanner = new Scanner(first);
-            scanner.useLocale(locale);
-            firstValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-        }
-
-        try{
-            Scanner scanner = new Scanner(second);
-            scanner.useLocale(locale);
-            secondValue = scanner.nextDouble();
-        }catch (InputMismatchException e){
-            throw  new InvalidCalculatorInputFormat();
-
-        }
-        return firstValue / secondValue;
+        firstValue = getFormattedValue(first, locale);
+        secondValue = getFormattedValue(second, locale);
+        double calcValue = firstValue / secondValue;
+        mMemory.put("m"+mMemory.size(),calcValue);
+        return calcValue;
     }
 
     public String getNthFibonacci(String value) {
@@ -113,21 +60,32 @@ public class CalculatorService {
         BigInteger secondFib = BigInteger.valueOf(1);
         BigInteger currentFib = BigInteger.valueOf(0);
         int fibSteps = Integer.valueOf(value);
-
-        if (value == "0" || value == "1"){
+        if (value.equals("0")|| value.equals("1")) {
             return value;
         }
-
-
-        for(int i = 0; i < fibSteps -1; i++){
+        for (int i = 0; i < fibSteps - 1; i++) {
             currentFib = firstFib.add(secondFib);
             firstFib = secondFib;
             secondFib = currentFib;
         }
-        System.out.println("CurrentFib: " + currentFib);
         return String.valueOf(currentFib);
 
 
+    }
 
+    private double getFormattedValue(String value, Locale locale) throws InvalidCalculatorInputFormat {
+        double firstValue;
+        try {
+            Scanner scanner = new Scanner(value);
+            scanner.useLocale(locale);
+            firstValue = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            throw new InvalidCalculatorInputFormat();
+        }
+        return firstValue;
+    }
+
+    public double getMemValue(String m1) {
+        return mMemory.get(m1);
     }
 }
--- test/se/nios/scrum/calculator/CalculatorServiceTest.java
+++ test/se/nios/scrum/calculator/CalculatorServiceTest.java
@@ -1,78 +1,109 @@
 package se.nios.scrum.calculator;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
 
 public class CalculatorServiceTest {
     private CalculatorService calculatorService;
+    private Map<String, String> mFibonacciList;
 
     @Before
-    public void init(){
+    public void init() {
         calculatorService = new CalculatorService();
-
+        mFibonacciList = new HashMap<>();
     }
 
     @Test
     public void testAddition() {
         double calculatedValue = 0;
         try {
-            calculatedValue = calculatorService.add("200.25", "200.75");
+            calculatedValue = calculatorService.add("200,25", "200,75");
         } catch (InvalidCalculatorInputFormat invalidCalculatorInputFormat) {
             invalidCalculatorInputFormat.printStackTrace();
         }
-        System.out.println(calculatedValue);
-        assertTrue("did not work", calculatedValue  == 200.25+200.75);
+        assertTrue("did not work", calculatedValue == 200.25 + 200.75);
     }
 
     @Test
-    public void testSubtraction(){
+    public void testSubtraction() {
         double calculatedValue = 0;
         try {
-            calculatedValue = calculatorService.subtract("124","200");
+            calculatedValue = calculatorService.subtract("124", "200");
         } catch (InvalidCalculatorInputFormat invalidCalculatorInputFormat) {
             invalidCalculatorInputFormat.printStackTrace();
         }
-        System.out.println(calculatedValue);
-        assertTrue("did not work", calculatedValue == 124-200);
+        assertTrue("did not work", calculatedValue == 124 - 200);
     }
 
     @Test
-    public void testMultiplication(){
+    public void testMultiplication() {
         double calculatedValue = 0;
         try {
-            calculatedValue = calculatorService.multiply("10.1","100.75");
+            calculatedValue = calculatorService.multiply("10,1", "100,75");
         } catch (InvalidCalculatorInputFormat invalidCalculatorInputFormat) {
             invalidCalculatorInputFormat.printStackTrace();
         }
-        System.out.println(calculatedValue);
-        assertTrue("Multiplication failed", calculatedValue == 10.1*100.75);
-
+        assertTrue("Multiplication failed", calculatedValue == 10.1 * 100.75);
     }
 
     @Test
-    public void testDivision(){
+    public void testDivision() {
         double calculatedValue = 0;
         try {
-            calculatedValue = calculatorService.divide("100", "10.2");
+            calculatedValue = calculatorService.divide("100", "10,2");
+        } catch (InvalidCalculatorInputFormat invalidCalculatorInputFormat) {
+            invalidCalculatorInputFormat.printStackTrace();
+        }
+        assertTrue("Division failed", calculatedValue == 100D / 10.2D);
+    }
+
+    @Test
+    public void testGetMemValue() {
+        double calculatedValue = 0;
+        try {
+            calculatedValue = calculatorService.add("10", "10");
         } catch (InvalidCalculatorInputFormat invalidCalculatorInputFormat) {
             invalidCalculatorInputFormat.printStackTrace();
         }
 
-        System.out.println(calculatedValue);
-        assertTrue("Division failed", calculatedValue == 100D/10.2D);
+        assertTrue("Memory value not present", calculatedValue == calculatorService.getMemValue("m0"));
     }
 
     @Test
     public void testNthFibonacci() {
         String calculatedValue = "";
-        calculatedValue = calculatorService.getNthFibonacci("300");
+        String fibListValue = "";
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\radia\\IdeaProjects\\Laboration1TDD\\test\\se\\nios\\scrum\\calculator\\fibonacci.list"))) {
+            String currentLine = br.readLine();
+            while (currentLine != null) {
+                String fibNumber = currentLine.substring(0, currentLine.indexOf(':'));
+                String fibValue = currentLine.substring(currentLine.indexOf(':') + 1);
+                fibNumber = fibNumber.trim();
+                fibValue = fibValue.trim();
+                mFibonacciList.put(fibNumber, fibValue);
+                currentLine = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < mFibonacciList.size(); i++) {
+            calculatedValue = calculatorService.getNthFibonacci(String.valueOf(i));
+            fibListValue = mFibonacciList.get(String.valueOf(i));
+            assertTrue("Fibonacci numbers doesn't match!", calculatedValue.equals(fibListValue));
+        }
     }
-
-
-
-
 }
